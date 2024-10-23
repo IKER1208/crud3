41324a96d549c6750c03b17db44b427cafe6412f
noahx001 <lopeznoe191119@gmail.com>
2024-10-22 20:07:37 -0600

    Prestamo Listo

--- app/Http/Controllers/PrestamoController.php
+++ app/Http/Controllers/PrestamoController.php
@@ -13,14 +13,11 @@ class PrestamoController extends Controller
     public function index(Request $request)
     {
         // Extraer el token desde el encabezado 'token_noe'
-        $token_noe = request()->header('token_noe');
+        $token_iker = request()->header('Authorization');
 
-        // Validar si el token fue proporcionado
-        if (!$token_noe) {
-            return response()->json([
-                'error' => 'Token no proporcionado'
-            ], 401); // Devuelve error 401 si el token no fue proporcionado
-        }
+        // Buscar el token correspondiente
+        $token_noe = Token::where('token_1', $token_iker)->first();
+        $token_noe = $token_noe->token_2;
 
         try {
             // Obtener todos los préstamos desde la base de datos
@@ -28,21 +25,14 @@ class PrestamoController extends Controller
 
             // Hacer la petición a la API externa utilizando el token proporcionado
             $dataResponse = Http::withHeaders([
-            'Authorization' => $token_noe
-            ])->get('https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/playlists');
-
-            // Verificar si la respuesta de la API falló
-            if ($dataResponse->failed()) {
-                return response()->json([
-                    'error' => 'Error al obtener datos de la API externa'
-                ], 400);
-            }
+                'Authorization' => "Bearer {$token_noe}"
+            ])->get('https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles');
 
             // Devolver la respuesta con los préstamos y las playlists de la API externa
             return response()->json([
                 'msg' => 'Listado de préstamos',
                 'prestamos' => $prestamos,
-                'playlists' => $dataResponse->json()
+                'singles' => $dataResponse->json()
             ], 200);
 
         } catch (\Exception $e) {
@@ -55,36 +45,27 @@ class PrestamoController extends Controller
 
     public function show($id, Request $request)
     {
-        // Extraer el token desde el encabezado 'token_noe'
-        $token_noe = request()->header('token_noe');
-
-        // Validar si el token fue proporcionado
-        if (!$token_noe) {
-            return response()->json([
-                'error' => 'Token no proporcionado'
-            ], 401); // Devuelve error 401 si el token no fue proporcionado
-        }
-
-        // Buscar el préstamo por su ID
-        $prestamo = Prestamo::find($id);
-        if (!$prestamo) {
-            return response()->json([
-                'msg' => 'No se encontró el préstamo'
-            ], 404);
-        }
-
         try {
+
+            // Buscar el préstamo por su ID
+            $prestamo = Prestamo::find($id);
+            // Extraer el token desde el encabezado 'token_noe'
+            $token_iker = $request->header('Authorization');
+
+            // Buscar el token correspondiente
+            $token_noe = Token::where('token_1', $token_iker)->first();
+            $token_noe = $token_noe->token_2;
+
+            if (!$prestamo) {
+                return response()->json([
+                    'msg' => 'No se encontró el prestamo'
+                ], 404);
+            }
+
             // Hacer la petición a la API externa utilizando el token proporcionado
             $dataResponse = Http::withHeaders([
-            'Authorization' => $token_noe
-            ])->get('https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/playlists/' . $id);
-
-            // Verificar si la respuesta de la API falló
-            if ($dataResponse->failed()) {
-                return response()->json([
-                    'error' => 'Error al obtener datos de la API externa'
-                ], 400);
-            }
+                'Authorization' => "Bearer {$token_noe}"
+            ])->get('https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles/' . $id);
 
             // Devolver la respuesta con el préstamo y los datos de la API externa
             return response()->json([
@@ -104,48 +85,74 @@ class PrestamoController extends Controller
     public function store(Request $request)
     {
         try {
-            // Validar la solicitud
-            $request->validate([
+            $token_iker = $request->header('Authorization');
+
+            // Buscar el token más reciente correspondiente
+            $token_noe_record = Token::where('token_1', $token_iker)
+                ->orderBy('created_at', 'desc')
+                ->first();
+
+            // Verificar si se encontró el token
+            if (!$token_noe_record) {
+                return response()->json(['error' => 'Token no encontrado'], 401);
+            }
+
+            $token_noe = $token_noe_record->token_2;
+
+            $validate = Validator::make($request->all(), [
                 'cliente_id' => 'required|integer',
-                'libro_id' => 'required',
+                'libro_id' => 'required|integer',
                 'fecha_prestamo' => 'required|date',
                 'fecha_devolucion' => 'nullable|date',
             ]);
 
+            if ($validate->fails()) {
+                return response()->json(['error' => $validate->errors()], 400);
+            }
+
             // Crear una instancia de Faker
             $faker = Faker::create();
 
-            // Crear un nuevo préstamo
-            $prestamo = new Prestamo();
-            $prestamo->cliente_id = $request->cliente_id;
-            $prestamo->libro_id = $request->libro_id;
-            $prestamo->fecha_prestamo = $request->fecha_prestamo;
-            $prestamo->fecha_devolucion = $request->fecha_devolucion;
-            $prestamo->save();
+            $formatos = ["CD", "Vinilo", "Cassette", "Digital"];
+
+            $formato = $formatos[array_rand($formatos)];
 
             // Hacer la petición a la API externa utilizando el token proporcionado
-            $token_noe = $request->header('token_noe');
             $dataResponse = Http::withHeaders([
-            'Authorization' => $token_noe
-            ])->post("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/playlists", [
-                'nombre' => $faker->sentence,
-                'descripcion' => $faker->sentence,
-                'user_id' => 1,
-            ]);
+                'Authorization' => "Bearer {$token_noe}"
+            ])->post('https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles', [
+                        'nombre' => $faker->name, // Usar nombre del request
+                        'formato' => $formato, // Usar formato del request
+                    ]);
 
-            // Verificar si la respuesta de la API falló
             if ($dataResponse->failed()) {
                 return response()->json([
-                    'error' => 'Error al crear la playlist en la API externa',
+                    'error' => 'Error al crear elSingleNode en la API externa',
                     'details' => $dataResponse->json()
                 ], 400);
             }
+
+
+            $dataResponse = Http::withHeaders([
+                'Authorization' => $token_noe
+            ])->post("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles", [
+                        'nombre' => $faker->sentence,
+                        'descripcion' => $faker->sentence,
+                        'user_id' => 1,
+                    ]);
+
+            $prestamo = Prestamo::create([
+                'cliente_id' => $request->input('cliente_id'),
+                'libro_id' => $request->input('libro_id'),
+                'fecha_prestamo' => $request->input('fecha_prestamo'),
+                'fecha_devolucion' => $request->input('fecha_devolucion'),
+            ]);
 
             // Devolver la respuesta confirmando la creación
             return response()->json([
                 'msg' => 'Préstamo creado con éxito',
                 'prestamo' => $prestamo,
-                'playlist' => $dataResponse->json()
+                'single' => $dataResponse->json()
             ], 201);
 
         } catch (\Exception $e) {
@@ -158,52 +165,76 @@ class PrestamoController extends Controller
     public function update(Request $request, $id)
     {
         try {
-            // Validar la solicitud
-            $request->validate([
-                'cliente_id' => 'required|exists:clientes,id',
-                'libro_id' => 'required|exists:libros,id',
+
+            $token_iker = $request->header('Authorization');
+
+            // Buscar el token más reciente correspondiente
+            $token_noe_record = Token::where('token_1', $token_iker)
+                ->orderBy('created_at', 'desc')
+                ->first();
+
+            // Verificar si se encontró el token
+            if (!$token_noe_record) {
+                return response()->json(['error' => 'Token no encontrado'], 401);
+            }
+
+            $token_noe = $token_noe_record->token_2;
+
+
+            $validate = Validator::make($request->all(), [
+                'cliente_id' => 'required|integer',
+                'libro_id' => 'required|integer',
                 'fecha_prestamo' => 'required|date',
                 'fecha_devolucion' => 'nullable|date',
             ]);
 
+            if ($validate->fails()) {
+                return response()->json(['error' => $validate->errors()], 400);
+            }
+
+            $prestamo = Prestamo::find($id);
+
+            if (!$prestamo) {
+                return response()->json([
+                    'msg' => 'No se encontró el prestamo'
+                ], 404);
+            }
+
             // Buscar el préstamo por su ID
             $prestamo = Prestamo::find($id);
+
             if (!$prestamo) {
                 return response()->json([
                     'msg' => 'No se encontró el préstamo'
                 ], 404);
             }
+
             $faker = Faker::create();
-            // Actualizar los datos del préstamo
-            $prestamo->cliente_id = $request->input('cliente_id');
-            $prestamo->libro_id = $request->input('libro_id');
-            $prestamo->fecha_prestamo = $request->input('fecha_prestamo');
-            $prestamo->fecha_devolucion = $request->input('fecha_devolucion');
-            $prestamo->save();
+
+            $formatos = ["CD", "Vinilo", "Cassette", "Digital"];
+
+            $formato = $formatos[array_rand($formatos)];
 
             // Hacer la petición a la API externa utilizando el token proporcionado
-            $token_noe = $request->header('token_noe');
             $dataResponse = Http::withHeaders([
-            'Authorization' => $token_noe
-            ])->put("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/playlists/".$id, [
-                'nombre' => $faker->sentence,
-                'descripcion' => $faker->sentence,
-                'user_id' => 1,
-            ]);
+                'Authorization' => "Bearer {$token_noe}"
+            ])->put("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles/" . $id, [
+                        'nombre' => $faker->sentence,
+                        'formato' => $formato,
+                    ]);
 
-            // Verificar si la respuesta de la API falló
-            if ($dataResponse->failed()) {
-                return response()->json([
-                    'error' => 'Error al actualizar la playlist en la API externa',
-                    'details' => $dataResponse->json()
-                ], 400);
-            }
+            $prestamo->update([
+                'cliente_id' => $request->input('cliente_id'),
+                'libro_id' => $request->input('libro_id'),
+                'fecha_prestamo' => $request->input('fecha_prestamo'),
+                'fecha_devolucion' => $request->input('fecha_devolucion'),
+            ]);
 
             // Devolver la respuesta confirmando la actualización
             return response()->json([
-                'msg' => 'Préstamo y playlist actualizados con éxito',
+                'msg' => 'Préstamo y single actualizados con éxito',
                 'prestamo' => $prestamo,
-                'playlist' => $dataResponse->json()
+                'singles' => $dataResponse->json()
             ], 200);
 
         } catch (\Exception $e) {
@@ -216,42 +247,48 @@ class PrestamoController extends Controller
     public function destroy(Request $request, $id)
     {
         try {
+
+            $token_iker = $request->header('Authorization');
+
+            // Buscar el token más reciente correspondiente
+            $token_noe_record = Token::where('token_1', $token_iker)
+                ->orderBy('created_at', 'desc')
+                ->first();
+
+            // Verificar si se encontró el token
+            if (!$token_noe_record) {
+                return response()->json(['error' => 'Token no encontrado'], 401);
+            }
+
+            $token_noe = $token_noe_record->token_2;
+
             // Buscar el préstamo por su ID
             $prestamo = Prestamo::find($id);
+
             if (!$prestamo) {
                 return response()->json([
                     'error' => 'Préstamo no encontrado'
                 ], 404);
             }
 
-            // Hacer la petición a la API externa utilizando el token proporcionado
-            $token_noe = $request->header('token_noe');
-
-            if (!$token_noe) {
-                return response()->json([
-                    'error' => 'Token no proporcionado'
-                ], 401);
-            }
-
-            // Eliminar el préstamo localmente
-            $prestamo->delete();
-
             // Eliminar la playlist en la API externa
             $dataResponse = Http::withHeaders([
-            'Authorization' => $token_noe
-            ])->delete("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/playlists/{$id}");
+                'Authorization' => "Bearer {$token_noe}"
+            ])->delete("https://710e-2806-101e-b-2c16-7424-7dea-e6e6-4762.ngrok-free.app/singles/{$id}");
 
             // Verificar si la respuesta de la API falló
             if ($dataResponse->failed()) {
                 return response()->json([
-                    'error' => 'Error al eliminar la playlist en la API externa',
                     'details' => $dataResponse->json()
-                ], 400);
+                ], $dataResponse->status());
             }
+
+            // Eliminar el registro de la base de datos
+            $prestamo->delete();
 
             // Devolver la respuesta confirmando la eliminación
             return response()->json([
-                'msg' => 'Préstamo y playlist eliminados con éxito'
+                'msg' => 'Préstamo y single eliminados con éxito'
             ], 200);
 
         } catch (\Exception $e) {
